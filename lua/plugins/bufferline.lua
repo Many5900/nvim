af9ece7a360cc865e11d4bdda7d8d2fefffac0f1
Marc Nygaard <marcnygaard100@gmail.com>
2024-04-16 18:53:19 +0200

    small change

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,11 +1,27 @@
 return {
+  {
     "akinsho/bufferline.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
     version = "*",
-    opts = {
-      options = {
-        mode = "tabs",
-        separator_style = "slant",
-      },
+    dependencies = "nvim-tree/nvim-web-devicons",
+    event = "BufReadPost",
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+      { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
+      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
+      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
+      { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+      { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
     },
-  }
+    config = function()
+      local bufferline = require("bufferline")
+      bufferline.setup({
+        options = {
+          themable = true,
+          style_preset = bufferline.style_preset.default,
+          always_show_bufferline = false,
+        },
+      })
+    end,
+  },
+}
