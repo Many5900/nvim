05f90e1f82ca028bf9ba189abd22c197e67ed321
Marc Nygaard <marcnygaard100@gmail.com>
2024-11-22 12:26:12 +0100

    .

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,9 +1,15 @@
 -- ************************************* --
 -- ********** SYSTEM SETTINGS ********** --
 -- ************************************* --
-vim.opt.clipboard = "unnamedplus"       -- Sync with system clipboard
-vim.opt.relativenumber = true           -- Enable relative line numbers
-vim.opt.number = true                   -- Enable absolute line number for the current line
-vim.opt.scrolloff = 12                  -- Scroll offset
-vim.opt.tabstop = 2                     -- Tabsize
+vim.opt.clipboard = "unnamedplus" -- Sync with system clipboard
+vim.opt.relativenumber = true -- Enable relative line numbers
+vim.opt.number = true -- Enable absolute line number for the current line
+vim.opt.scrolloff = 12 -- Scroll offset
+-- vim.opt.tabstop = 2                     -- Tabsize
 -- vim.opt.textwidth = 80                  -- Automatically wrap text at 80 characters
+
+-- Set tab size to 4 spaces
+vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for
+vim.opt.softtabstop = 4 -- Number of spaces that a <Tab> counts for while performing editing operations
+vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true -- Use spaces instead of tabs
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,36 +1,42 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "rust_analyzer", "tailwindcss", "cssls", "html", "htmx", "eslint", "ts_ls", 
-                }
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities() -- FOR THE CMP FILE!!!
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"rust_analyzer",
+					"tailwindcss",
+					"cssls",
+					"html",
+					"htmx",
+					"eslint",
+					"ts_ls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities() -- FOR THE CMP FILE!!!
 
-            local lspconfig = require("lspconfig")
-        
-            lspconfig.rust_analyzer.setup({ capabilities = capabilities })
-            lspconfig.lua_ls.setup({ capabilities = capabilities })
-            lspconfig.tailwindcss.setup({ capabilities = capabilities })
-            lspconfig.cssls.setup({ capabilities = capabilities })
-            lspconfig.html.setup({ capabilities = capabilities })
-            lspconfig.htmx.setup({ capabilities = capabilities })
-            lspconfig.eslint.setup({ capabilities = capabilities })
-            lspconfig.ts_ls.setup({ capabilities = capabilities })
+			local lspconfig = require("lspconfig")
 
-        end
-    }
+			lspconfig.rust_analyzer.setup({ capabilities = capabilities })
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.tailwindcss.setup({ capabilities = capabilities })
+			lspconfig.cssls.setup({ capabilities = capabilities })
+			lspconfig.html.setup({ capabilities = capabilities })
+			lspconfig.htmx.setup({ capabilities = capabilities })
+			lspconfig.eslint.setup({ capabilities = capabilities })
+			lspconfig.ts_ls.setup({ capabilities = capabilities })
+			lspconfig.svelte.setup({ capabilities = capabilities })
+		end,
+	},
 }
